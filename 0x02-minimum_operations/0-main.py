7d14c30a15df2087c3d171467d7f2dfad681eb35
Godd-hand <interstellar300000000@gmail.com>
2023-02-01 23:52:34 +0000

    Minimum operations

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,16 @@
+in file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+in file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 7
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
